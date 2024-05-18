620ce04dd3fc2d22535a599346c14628e43fdec5
feigebaobei <18515195415@163.com>
2024-05-18 22:06:37 +0800

    feat(schedule): 每2min删除一次验证码

--- src/schedule.ts
+++ src/schedule.ts
@@ -6,9 +6,24 @@ let start = function() {
     // clog('start')
     // 这里的时长应与accessToken的有效时长相关。
     // jobTime = tokenDuration * 4
-    let job = schedule.scheduleJob('* * */2 * * *', function() {
+    // *    *    *    *    *    *
+    // ┬    ┬    ┬    ┬    ┬    ┬
+    // │    │    │    │    │    │
+    // │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
+    // │    │    │    │    └───── month (1 - 12)
+    // │    │    │    └────────── day of month (1 - 31)
+    // │    │    └─────────────── hour (0 - 23)
+    // │    └──────────────────── minute (0 - 59)
+    // └───────────────────────── second (0 - 59, OPTIONAL)
+
+    // let job = schedule.scheduleJob('* * */2 * * *', function() {
+    //     let now = new Date().getTime()
+    //     usersDb.collection('black_list').deleteMany({expires: {$lt: now}})
+    // })
+    
+    let cleanVerificationCode = schedule.scheduleJob('* * * * */2 *', () => {
         let now = new Date().getTime()
-        usersDb.collection('black_list').deleteMany({expires: {$lt: now}})
+        usersDb.collection('verification_code').deleteMany({expiredTime: {$lt: now}})
     })
 }
 export default start
